6b398b485cff47db538fd6972e7d48acf5809907
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-08-28 01:43:51 +0530

    Create 04_numpyramid2.java

---
+++ Patterns/04_numpyramid2.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	   
+	for(int i=1;i<=n;i++){     //loop1 for rows
+	    for(int j=1;j<=i;j++){  //loop2 to print i
+	        	System.out.print(i);
+	    }System.out.println();
+	}
+	}
+}
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
