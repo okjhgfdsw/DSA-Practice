8fe0d59cc7f0eafc09ef1c66655e74f44e6b386f
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-08-28 02:03:23 +0530

    Update 05_invertpyramid.java

--- Patterns/05_invertpyramid.java
+++ Patterns/05_invertpyramid.java
@@ -2,9 +2,9 @@ public class Main
 {
 	public static void main(String[] args) {
 	    int n=5;
-	   
+	   //always try to use i=0 amd n-1 in looping problem to avoid complexity
 	for(int i=0;i<=n-1;i++){     //loop1 for rows
-	    for(int j=0;j<=n-1-i;j++){ 
+	    for(int j=0;j<=n-1-i;j++){ // loop2 have logic n-1-i dont do i-- 
 	        	System.out.print("*");
 	    }System.out.println();
 	}
