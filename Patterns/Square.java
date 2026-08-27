848217c99c12e056bb290d1590aaaf681a41cacf
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-08-28 01:32:38 +0530

    Update Square.java

--- Patterns/Square.java
+++ Patterns/Square.java
@@ -3,8 +3,8 @@ public class Main
 	public static void main(String[] args) {
 	    int n=5;
 	    int m=5;
-	for(int i=0;i<=n;i++){
-	    for(int j=0;j<=m;j++){
+	for(int i=0;i<=n-1;i++){
+	    for(int j=0;j<=m-1;j++){
 	        	System.out.print("*");
 	    }System.out.println();
 	}
@@ -16,5 +16,5 @@ public class Main
 // ******
 // ******
 // ******
-// ******
+
 
