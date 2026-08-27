91f09570bbc81ca75c907aaa71a1c1298f684138
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-08-28 02:00:04 +0530

    Create 05_invertpyramid.java

---
+++ Patterns/05_invertpyramid.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	   
+	for(int i=0;i<=n-1;i++){     //loop1 for rows
+	    for(int j=0;j<=n-1-i;j++){ 
+	        	System.out.print("*");
+	    }System.out.println();
+	}
+	}
+}
+// *****
+// ****
+// ***
+// **
+// *
