ac044e20b2336d7ffedad59ffb5adeaa587e00e7
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-08-28 01:42:15 +0530

    Update 03_numpyramid.java

--- Patterns/03_numpyramid.java
+++ Patterns/03_numpyramid.java
@@ -4,7 +4,7 @@ ublic class Main
 	    int n=5;
 	   
 	for(int i=1;i<=n;i++){     //loop1 for rows
-	    for(int j=1;j<=i;j++){  //loop2 to print *
+	    for(int j=1;j<=i;j++){  //loop2 to print j
 	        	System.out.print(j);
 	    }System.out.println();
 	}
