cd9c9d04656248b29c8d6c8a1fd10a5edc62ae2e
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-08-28 01:31:47 +0530

    Create pyramid.java

---
+++ Patterns/pyramid.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	   
+	for(int i=1;i<=n;i++){     //loop1 for rows
+	    for(int j=1;j<=i;j++){  //loop2 to print *
+	        	System.out.print("*");
+	    }System.out.println();
+	}
+	}
+}
+//output:
+// *
+// **
+// ***
+// ****
+// *****
