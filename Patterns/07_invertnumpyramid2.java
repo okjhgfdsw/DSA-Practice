e91a686d681eef13ae34b22f1e9429cf6ab0e728
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-08-28 02:37:41 +0530

    Create 07_invertnumpyramid2.java

---
+++ Patterns/07_invertnumpyramid2.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	   
+	for(int i=5;i>=1;i--){     //loop1 for rows
+	    for(int j=1;j<=i;j++){ //loop2 every time phir se initiate karta from 1 when loop1 condition satisfied
+	        	System.out.print(i);//to write same element in a row
+	    }System.out.println();
+	}
+	}
+}
+// 55555
+// 4444
+// 333
+// 22
+// 1
