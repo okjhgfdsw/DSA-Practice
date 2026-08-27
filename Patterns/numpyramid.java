5bcd5a631c740be9036d43ad053c44627e3e54f1
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-08-28 01:34:52 +0530

    Create numpyramid.java

---
+++ Patterns/numpyramid.java
@@ -0,0 +1,18 @@
+ublic class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	   
+	for(int i=1;i<=n;i++){     //loop1 for rows
+	    for(int j=1;j<=i;j++){  //loop2 to print *
+	        	System.out.print(j);
+	    }System.out.println();
+	}
+	}
+}
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
